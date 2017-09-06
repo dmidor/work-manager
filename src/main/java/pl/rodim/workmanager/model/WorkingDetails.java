8de4bcf5523f575e69522d9093acf84663e70d5e
Damian Midor <damian.midor@pragmaticcoders.com>
2017-09-06 21:59:15 +0200

    few hints about lombok and challenge 2

--- src/main/java/pl/rodim/workmanager/model/WorkingDetails.java
+++ src/main/java/pl/rodim/workmanager/model/WorkingDetails.java
@@ -8,6 +8,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
 
+//TODO reade about lombok functionality https://projectlombok.org/
 @Builder
 @Getter
 public class WorkingDetails {
@@ -15,5 +16,7 @@ public class WorkingDetails {
     private List<LocalDate> daysOfWork;
     private List<DayOfWeek> excludeDaysOfWeek;
     private List<LocalDate> vacationsDays;
+
+    //challenge 2 try add bankHoliday dates. Start from test!
 
 }
